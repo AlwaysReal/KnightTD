24288b77825135120f9d7b4722810c1ca87a4f19
AlwaysReal <cwchanin26@wabash.edu>
2025-12-01 15:43:34 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include "raylib.h"
+#include "raymath.h"
+int main()
+{
+    // window dimensions
+    int windowDimensions[2];
+    windowDimensions[0] = 384;
+    windowDimensions[1] = 384;
+
+    InitWindow(windowDimensions[0], windowDimensions[1], "First Top-down");
+
+    Texture2D map = LoadTexture("nature_tileset/WorldMap.png");
+    Vector2 mapPos{0.0, 0.0};
+    float speed{4.0};
+
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2 knightPos{
+        (float)windowDimensions[0]/2.0f - 4.0f * (0.5f * (float)knight.width/6),
+        (float)windowDimensions[1]/2.0f - 4.0f * (0.5f * (float)knight.height)
+    };
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        Vector2 direction{};
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;
+        if (Vector2Length(direction) != 0.0)
+        {
+            // set mapPos = mapPos - direction
+            mapPos = Vector2Subtract(mapPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
+
+        // draw the map
+        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+
+        // draw the character
+        Rectangle knightSource{ 0.f, 0.f, (float)knight.width/6.f, (float)knight.height};
+        Rectangle knightDest{ knightPos.x, knightPos.y, 4.0f * knight.width/6.f, 4.0f * knight.height};
+        DrawTexturePro(knight,knightSource, knightDest, Vector2{}, 0.0, WHITE);
+       
+        EndDrawing();
+    }
+    UnloadTexture(map);
+    CloseWindow();
+}
